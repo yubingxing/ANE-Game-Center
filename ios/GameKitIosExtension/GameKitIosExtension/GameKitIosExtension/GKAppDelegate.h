2c9bb2db5e91fccf2eda40d244a5af592abefa97
IceStar <yubingxing123@gmail.com>
2012-09-26 08:08:00 +0800

    Refactor GameCenterIosExtension to GameKitIosExtension

---
+++ ios/GameKitIosExtension/GameKitIosExtension/GameKitIosExtension/GKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GKAppDelegate.h
+//  GameKitIosExtension
+//
+//  Created by letang on 9/26/12.
+//  Copyright (c) 2012 IceStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/GameKitIosExtension/GameKitIosExtension/GameKitIosExtension/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GameKitIosExtension
+//
+//  Created by letang on 9/26/12.
+//  Copyright (c) 2012 IceStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GKAppDelegate class]));
+    }
+}
